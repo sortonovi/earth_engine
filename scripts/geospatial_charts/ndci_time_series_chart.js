b48aee35b89dfbbb0fb41a62f6b60ba9162b27d2
Sarah <sarah.ortonovi@side-geomatics.com>
2025-05-03 07:22:31 +0200

    Update ndci_time_series_chart.js

--- scripts/geospatial_charts/ndci_time_series_chart.js
+++ scripts/geospatial_charts/ndci_time_series_chart.js
@@ -1,1 +1,13 @@
-
+var chart = ui.Chart.image.series({
+  imageCollection: sentinelCollection,
+  region: WaterBodyArea,
+  reducer: ee.Reducer.mean(),
+  scale: 10,
+  xProperty: 'system:time_start'
+}).setOptions({
+  title: 'CHLOROPHYLL INDEX (NDCI) TIME SERIES',
+  vAxis: {title: 'NDCI'},
+  hAxis: {title: 'Date'},
+  lineWidth: 2,
+  pointSize: 2
+});
